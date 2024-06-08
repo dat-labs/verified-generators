0f9d9e49dd0c03e0479570f8aa942f97fb99eab4
Rijumone Choudhuri <rijumone.choudhuri@decision-tree.com>
2024-06-08 14:45:38 +0530

    Cohere generator WIP

--- verified_generators/cohere/generator.py
+++ verified_generators/cohere/generator.py
@@ -1,15 +1,20 @@
 import os
-from typing import Any, Optional, Tuple
+from typing import Any, Optional, Tuple, Iterator
 import cohere
+from dat_core.pydantic_models.dat_message import DatMessage
 from dat_core.connectors.generators.base import GeneratorBase
 from dat_core.pydantic_models import ConnectorSpecification
 from verified_generators.cohere.specs import CohereSpecification
 
 
 class Cohere(GeneratorBase):
-    _spec_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'specs.yml')
-    
-    co = cohere.Client(api_key="YOUR_API_KEY")
+    _spec_file = os.path.join(os.path.dirname(
+        os.path.abspath(__file__)), 'specs.yml')
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.cohere_client = cohere.Client(api_key="YOUR_API_KEY")
+
     def check_connection(self, config: ConnectorSpecification) -> Tuple[bool, Optional[Any]]:
         # Implement your connection check logic here
         return False, {}
@@ -29,9 +34,11 @@ class Cohere(GeneratorBase):
         Yields:
             Iterator[Dict]: Each row should be wrapped around a DatMessage obj
         """
-
-        dat_message.record.data.vectors = OpenAIEmbeddings(
-            openai_api_key=config.connection_specification.openai_api_key,
+        input_type = "search_query"
+        dat_message.record.data.vectors = self.cohere_client.embed(
+            texts=dat_message.record.data.document_chunk,
             model=config.connection_specification.openai_model,
-        ).embed_query(dat_message.record.data.document_chunk)
+            input_type=input_type,
+            embedding_types=['float'],
+        )
         yield dat_message
